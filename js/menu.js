ce23a08028550b650a3742cbb6c8084dffea13d0
Анна Казаева <76570998+kazaeva2015@users.noreply.github.com>
2022-06-29 01:21:40 +0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+(() => {
+    $(".menu__item2").click(() => {
+        $(".s_menu").slideToggle();
+    });
+});
+
+$(() => {
+    $(".menu__item").click(() => {
+        $(".sub-menu").slideToggle();
+    });
+});
+
+$(() => {
+    $(".menu__item1").click(() => {
+        $(".sub-menu1").slideToggle();
+    });
+});
+
+
+
